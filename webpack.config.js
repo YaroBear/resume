2eaf76544cbc6add7476afc877ef5ac939e3823e
Yaroslav Berejnoi <berejnoi.yaroslav@gmail.com>
2023-11-28 21:30:47 -0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    liveReload: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/assets/", to: path.resolve(__dirname, "dist/assets") },
+      ],
+    }),
+  ],
+};
